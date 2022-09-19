ae2656e7f8b841cc656757087b097f4a60ea2e34
lucas <Lucasbriandbr@gmail.com>
2022-09-19 16:41:24 +0200

    AAAAAAAAAAAAAAAAAARGh

---
+++ web/components/AppBar.js
@@ -0,0 +1,17 @@
+import Link from 'next/link'
+import Button from './Button'
+import styles from '../styles/Home.module.css'
+
+export default function AppBar() {
+    return(
+        <nav className={`flex ${styles.navbar} fixed w-full flex-row p-4 gap-4 justify-between rounded-br-lg rounded-bl-lg items-center`}>
+            <Link href={'/'}>
+                <a className='text-sm text-black'>BDE - Campus Riera</a>
+            </Link>
+            <div className="flex flex-row gap-4">
+                <Button content={`Contact`} link={`/contact`}/>
+                <Button content={`Voter`} link={`/voter`}/>
+            </div>
+        </nav>
+    )
+}
