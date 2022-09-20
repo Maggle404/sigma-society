373a7284aaaf210e91448700701ef44d18661019
lucas <Lucasbriandbr@gmail.com>
2022-09-20 16:51:10 +0200

    Ajout du jsonwebtoken

--- web/components/Footer.js
+++ web/components/Footer.js
@@ -40,7 +40,7 @@ export default function Footer() {
                 </div>
             </div>
             <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-            <div class="flex items-center justify-center flex-col">
+            <div class="flex items-center justify-center flex-col"> 
                 <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© {date.getFullYear().toString()} - Lucas Briand - All Rights Reserved.
                 </span>
                 <div class="flex mt-4 space-x-6 sm:justify-center sm:mt-0">
